c27574ff263fa8be51fa078e4e19c0abcc8c55d9
ja-ho <magicuv@gmail.com>
2020-10-19 23:24:11 +0900

    [Backend EG]Member service done
    Signed-off-by: ja-ho <magicuv@gmail.com>

---
+++ src/main/java/Jaho/springtutorial/service/MemberService.java
@@ -0,0 +1,51 @@
+package Jaho.springtutorial.service;
+
+import Jaho.springtutorial.domain.Member;
+import Jaho.springtutorial.repository.MemberRepository;
+import Jaho.springtutorial.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    * 회원가입
+    * */
+    public Long join(Member member) {
+        //중복 회원 검증
+//        findByName으로 로직이 쭉 이어질 경우 리팩토링 측면에서 메서드로 빼주는 게 좋다.
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /*
+    * 전체 회원 조회
+    * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /*
+    * 개인 회원 조회
+    * */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
