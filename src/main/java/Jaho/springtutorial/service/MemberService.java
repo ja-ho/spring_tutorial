130dda4c2f38b319100ba584706197ee1ec295d9
ja-ho <magicuv@gmail.com>
2020-10-24 00:24:37 +0900

    [Spring Tutorial]Component Scan and Autowired
    Signed-off-by: ja-ho <magicuv@gmail.com>

--- src/main/java/Jaho/springtutorial/service/MemberService.java
+++ src/main/java/Jaho/springtutorial/service/MemberService.java
@@ -2,14 +2,19 @@ package Jaho.springtutorial.service;
 
 import Jaho.springtutorial.domain.Member;
 import Jaho.springtutorial.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
