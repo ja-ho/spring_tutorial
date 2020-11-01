cec1413499596d3c62cbfc7e8890bf513a0f52de
ja-ho <magicuv@gmail.com>
2020-11-01 19:14:05 +0900

    [Spring tutorial]pure Jdbc implements. JdbcMemberRepository.
    This is important because it never change the origin code.
    It just add JdbceMemberRepository implementint MemberRepository interface.
    Plus, it modify assembly. SpringConfig and etc..
    
    Signed-off-by: ja-ho <magicuv@gmail.com>

--- src/main/java/Jaho/springtutorial/SpringConfig.java
+++ src/main/java/Jaho/springtutorial/SpringConfig.java
@@ -1,13 +1,25 @@
 package Jaho.springtutorial;
 
+import Jaho.springtutorial.repository.JdbcMemberRepository;
 import Jaho.springtutorial.repository.MemberRepository;
-import Jaho.springtutorial.repository.MemoryMemberRepository;
 import Jaho.springtutorial.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    //DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다. 스프링 부트는 데이터베이스 커넥션 정
+    //보(application.properties)를 바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다.
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ src/main/java/Jaho/springtutorial/repository/JdbcMemberRepository.java
@@ -0,0 +1,147 @@
+package Jaho.springtutorial.repository;
+
+import Jaho.springtutorial.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository {
+    private final DataSource dataSource;
+    public JdbcMemberRepository(DataSource dataSource) {    //Dependendy injection spring boot이 datasource 만들어 놓은 것을 SpringConfig에서 injection 해놓았고 그것을 이용
+        this.dataSource = dataSource;
+    }
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);   //db에 만들어진 key값을 가져올 때
+            pstmt.setString(1, member.getName());   //첫 번째 ?에 값을 넣어줌
+            pstmt.executeUpdate();  //실제 sql update
+            rs = pstmt.getGeneratedKeys();  //key 값
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs); //db 연결은 외부 network와 연결이기 때문에 사용이 끝나면 바로바로 해제 해준다. resouce 해제
+        }
+    }
+    @Override
+    public Optional<Member> findById(Long id) {
+        String sql = "select * from member where id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            rs = pstmt.executeQuery();  //update 아니고 쿼리문일 때는 executeQuery
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+    @Override
+    public List<Member> findAll() {
+        String sql = "select * from member";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<Member> members = new ArrayList<>();
+            while(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                members.add(member);
+            }
+            return members;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+    @Override
+    public Optional<Member> findByName(String name) {
+        String sql = "select * from member where name = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+    //DataSourceUtils를 사용해서 connect 하고 close 하는 이유 : transaction 일관성? 위해. db와 연결, 끊기 할 때는 dataSource로 바로 하는 것이 아니라 dataSourceUtil을 사용한다.
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
+    {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //DataSourceUtils를 사용해서 connect 하고 close 하는 이유 : transaction 일관성? 위해. db와 연결, 끊기 할 때는 dataSource로 바로 하는 것이 아니라 dataSourceUtil을 사용한다.
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+}
